1dd94fe0ecac5cf21233d3ed0b2b02b0699e9ee1
qqin <qinshengqs@foxmail.com>
2017-10-16 15:23:37 +0800

    deepin_test

---
+++ src/test/GitTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by qqin on 17-10-16  下午3:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
